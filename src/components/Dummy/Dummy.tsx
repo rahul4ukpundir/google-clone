c4b883b160796f76794f2cb38c9045a931e7f2be
Rahul Pundir <81017489+rahul4ukpundir@users.noreply.github.com>
2025-11-12 18:30:54 +0530

    chore: apply AI plan for story
    [file] src/components/Dummy/Dummy.tsx

---
+++ src/components/Dummy/Dummy.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * A simple component that displays "Hello World!".
+ */
+const Dummy: React.FC = () => {
+  return (
+    <div>
+      Hello World!
+    </div>
+  );
+};
+
+export default Dummy;
